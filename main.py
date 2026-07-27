69912256d2badc88bbcc685d4a3b51912a408d51
IBR <u2024413@giki.edu.pk>
2026-07-27 18:05:03 +0500

    fixed a bug

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
 from utility.dbConnection import dbConfig
+from routes.user import routes
 import asyncio
 
 app = Flask(__name__)
+app.register_blueprint(routes)
 
 @app.route('/')
 async def home():
     mongo = dbConfig()
     print('user: ', list(mongo.cx['test']['users'].find({'userName':'asdf'})))
-    print('user: ', type(list(mongo.cx['test']['users'].find({'userName':'asdf'}))[0]))
+    print('user: ', type(list(mongo.cx['test']['users'].find({'userName':'asdf'}))))
     print(type(mongo))
 
     return jsonify({'message': 'this is message from flask home func yes'})
