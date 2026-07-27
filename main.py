cb163619b6719820e0303495810d5710c1ae553c
IBR <u2024413@giki.edu.pk>
2026-07-27 20:16:44 +0500

    added config for cookies

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from flask import Flask, jsonify
 from utility.dbConnection import dbConfig
 from routes.user import routes
 import asyncio
+import os
+from dotenv import load_dotenv
+from flask_jwt_extended import jwt_manager
 
+load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(routes)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_COOKIE_HTTPONLY'] = True
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+
+jwt = jwt_manager(app)
 
 @app.route('/')
 async def home():
