ba411453b4ce0b2f193be3c96b09e95747e1b2d4
IBR <u2024413@giki.edu.pk>
2026-07-29 15:41:00 +0500

    registered new blueprints for different routes

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
+from routes.auth import routes as auth_routes
+from routes.mediaPosts import routes as mediaposts_routes
 
 load_dotenv()
 app = Flask(__name__)
-app.register_blueprint(routes)
+app.register_blueprint(auth_routes)
+app.register_blueprint(mediaposts_routes)
 secretKey = os.getenv('JWT_SECRET_KEY')
 # print('secret key:', type(secretKey))
 app.config['JWT_SECRET_KEY'] = secretKey
