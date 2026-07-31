834230d9338de7e9224622c73d525cffaf4bef2d
IBR <u2024413@giki.edu.pk>
2026-07-31 15:46:44 +0500

    updated config to accept reqests from backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    Pages({
+      dirs: 'src/pages',
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: function(path) {return path.replace(/^\/api/, '')}
+      }
+    }
+  }
 })
