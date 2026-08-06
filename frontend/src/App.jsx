59ba2d314dbc53bae66767e07d2149064e8ac497
IBR <u2024413@giki.edu.pk>
2026-08-06 16:34:55 +0500

    applied toast provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/home/page";
 import Login from "./pages/login/page";
 import Logout from "./pages/logout/page";
 import Mediaposts from "./pages/mediaposts/page";
 import Profile from "./pages/profile/page";
 import Signup from "./pages/signup/page";
-
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
@@ -36,7 +36,12 @@ const router = createBrowserRouter([
 function App() {
   
   return (
-    <RouterProvider router={router}> </RouterProvider>
+    <>
+    <Toaster/>
+    <RouterProvider router={router}>
+      
+    </RouterProvider>
+    </>
 
   );
 }
