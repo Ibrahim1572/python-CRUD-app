5baab04278c1f019669633e9e76adcaa7b58ac4f
IBR <u2024413@giki.edu.pk>
2026-08-07 14:11:19 +0500

    fixed routing flow

--- frontend/src/pages/logout/page.tsx
+++ frontend/src/pages/logout/page.tsx
@@ -10,8 +10,20 @@ export default function Logout(){
     }
 
     return(
-        <div>
-            <button onClick={logoutFunc} > Logout</button>
+        <div className="font-sans flex flex-col justify-center justify-items-center items-center p-8">
+            <button
+                onClick={logoutFunc}
+                className="px-32 bg-transparent text-white border border-white/50 rounded-none py-4 text-[14.4px] font-light uppercase tracking-[0.2px] cursor-pointer transition-colors duration-150 hover:bg-[#1EAEDB] hover:opacity-70"
+            >
+                Logout
+            </button>
+            <br/>
+            <button
+                onClick={()=>{window.location.href = '/mediaposts'}}
+                className="px-25 bg-transparent text-white border border-white/50 rounded-none py-4 text-[14.4px] font-light uppercase tracking-[0.2px] cursor-pointer transition-colors duration-150 hover:bg-[#FFC000] hover:opacity-70"
+            >
+                Mediaposts Page
+            </button>
         </div>
     )
 }
