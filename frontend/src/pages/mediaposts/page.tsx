e9b8e00186faaa5e5ff4aba3a2086848e573dbcd
IBR <u2024413@giki.edu.pk>
2026-08-07 14:12:27 +0500

    removed profile view and added update post view

--- frontend/src/pages/mediaposts/page.tsx
+++ frontend/src/pages/mediaposts/page.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react"
-
 import AddPost from "./components/addPost";
 import DeletePost from "./components/deletePost";
 import UpdatePost from "./components/updatePost";
@@ -14,13 +13,12 @@ export default function Mediaposts(){
     const condititionalRenderingComponents = () =>{
         let jsx
         switch (currentView) {
-            case 'profile':
-                break;
-
             case 'updatePost':
+                return <UpdatePost/>
                 break;
 
             case 'deletePost':
+                return <DeletePost/>
                 break;
 
             case 'viewOne':
@@ -32,6 +30,7 @@ export default function Mediaposts(){
                 break;
 
             case 'viewArchived':
+                return <ViewArchived/>
                 break;
         
             default:
@@ -46,7 +45,7 @@ export default function Mediaposts(){
     return (
         <div className="flex flex-col gap-y-16 min-h-screen w-full bg-black font-sans">
             <div className="flex flex-row flex-wrap gap-3 p-6 bg-[#181818] items-center justify-center border-b border-[#202020]">
-                <div onClick={()=>{setCurrentView('profile')}} className="bg-[#FFC000] text-black px-6 py-4 text-sm font-normal uppercase tracking-normal rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#917300]">
+                <div onClick={()=>{window.location.href='/profile'}} className="bg-[#FFC000] text-black px-6 py-4 text-sm font-normal uppercase tracking-normal rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#917300]">
                     Profile
                 </div>
                 <div onClick={()=>{setCurrentView('addPost')}} className="bg-transparent text-white border border-white/50 px-4 py-4 text-[14.4px] font-light uppercase tracking-[0.2px] rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#1EAEDB] hover:opacity-70">
@@ -67,7 +66,7 @@ export default function Mediaposts(){
                 <div onClick={()=>{setCurrentView('viewArchived')}} className="bg-transparent text-white border border-white/50 px-4 py-4 text-[14.4px] font-light uppercase tracking-[0.2px] rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#1EAEDB] hover:opacity-70">
                     View Archived
                 </div>
-                <div onClick={()=>{setCurrentView('logout')}} className="bg-[#969696] text-[#202020] px-4 py-4 text-[13px] font-light uppercase tracking-[0.13px] rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#7D7D7D]">
+                <div onClick={()=>{window.location.href='/logout'}} className="bg-[#969696] text-[#202020] px-4 py-4 text-[13px] font-light uppercase tracking-[0.13px] rounded-none cursor-pointer transition-colors duration-150 hover:bg-[#7D7D7D]">
                     Logout
                 </div>
             </div>
